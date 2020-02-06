d88e0bf04ef8f95fe9eb96aceec5b50cd401d72e
SelahattinKaanDeniz <selahattinkaandeniz@etu.edu.tr>
2020-02-07 00:41:26 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,6 +2,14 @@
 	package com.mycompany.app;
 	
 	import java.util.ArrayList;
+	import static spark.Spark.get;
+	import static spark.Spark.port;
+	import static spark.Spark.post;
+	import java.util.HashMap;
+	import java.util.Map;
+	import spark.ModelAndView;
+	import spark.template.mustache.MustacheTemplateEngine;
+
 	
 	/**
 	 * Hello world!
@@ -60,9 +68,51 @@
 	
 		
 	    public static void main( String[] args )
-	    {
-	       
-	        ArrayList<Integer> x = new ArrayList<>(); 
+	    {    
+	    	 port(getHerokuAssignedPort());
+	    	 get("/", (req, res) -> "Counting Occurences");
+	    	 post("/compute", (req, res) -> {
+	             //System.out.println(req.queryParams("input1"));
+	             //System.out.println(req.queryParams("input2"));
+
+	             String input1 = req.queryParams("input1");
+	             java.util.Scanner sc1 = new java.util.Scanner(input1);
+	             sc1.useDelimiter("[;\r\n]+");
+	             java.util.ArrayList<Integer> inputList1 = new java.util.ArrayList<>();
+	             while (sc1.hasNext())
+	             {
+	               int value1 = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+	               inputList1.add(value1);
+	             }
+	             String input2 = req.queryParams("input2");
+	             java.util.Scanner sc2 = new java.util.Scanner(input2);
+	             sc2.useDelimiter("[;\r\n]+");
+	             java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+	             while (sc2.hasNext())
+	             {
+	               int value2 = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+	               inputList2.add(value2);
+	             }
+	             String int1 = req.queryParams("int1").replaceAll("\\s","");
+	             int int1AsInt = Integer.parseInt(int1);
+	             String int2 = req.queryParams("int2").replaceAll("\\s","");
+	             int int2AsInt = Integer.parseInt(int2);
+	             boolean result = App.countOccur(inputList1,inputList2,int1AsInt,int2AsInt);
+
+	             Map map = new HashMap();
+	              map.put("result", result);
+	              return new ModelAndView(map, "compute.mustache");
+	            }, new MustacheTemplateEngine());
+
+
+
+
+	        
+	    	
+	    	
+	    	
+	    	
+	    	/*ArrayList<Integer> x = new ArrayList<>(); 
 	        x.add(1); 
 	        x.add(2); 
 	        x.add(3); 
@@ -79,6 +129,15 @@
 	        
 	        System.out.println("Integer parameters must be in the ArrayLists");
 	        
-	        App.countOccur(x, y,1,4);
+	        App.countOccur(x, y,1,4);*/
 	    }
+	    static int getHerokuAssignedPort() {
+	        ProcessBuilder processBuilder = new ProcessBuilder();
+	        if (processBuilder.environment().get("PORT") != null) {
+	            return Integer.parseInt(processBuilder.environment().get("PORT"));
+	        }
+	        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+	    }
+
+
 	}
